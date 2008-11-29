c0d984a7c80599a9ad562daeeb82ed9ef471184b
Can Berk Güder <cbguder@gmail.com>
2008-11-29 17:54:42 +0200

    License and Copyright info.

--- TuxMate/Main.cs
+++ TuxMate/Main.cs
@@ -1,6 +1,21 @@
-// Main.cs created with MonoDevelop
-// User: cbguder at 11:59 AM 11/9/2008
+// Main.cs
 //
+// Copyright (C) 2008 Can Berk Güder
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 using System;
 using Gtk;
 
